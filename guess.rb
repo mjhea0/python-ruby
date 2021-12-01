76898ea4d04d46ba9a80747aca0416168e93de9d
tiosgz <alamica@protonmail.com>
2021-12-01 20:11:06 +0000

    Depythonize ruby example
    Some other rubyists may disagree with my version, & i encourage them to
    fix it. However, it's definitely more rubyish than the previous version.

--- guess.rb
+++ guess.rb
@@ -1,30 +1,20 @@
 number = rand(1..20)
-guesses = 0
 
 puts 'Hello! What is your name?'
-name = gets.chomp.to_s
+name = gets&.chomp
 
-puts "Hi, #{name}. I'm thinking of a number between 1 and 20." 
+puts "Hi, #{name}. I'm thinking of a number between 1 and 20."
 
-while guesses < 6
+1.upto 6 do |guesses|
+  puts "What is your guess? You have #{7 - guesses} more guesses."
+  guess = gets&.chomp.to_i
 
-  puts "What is your guess? You have #{6-guesses} more guesses."
-  guess = gets.chomp.to_i
-  guesses += 1
-
-  unless guess == number
-    message = if guess > number
-                "Too high"
-              else
-                "Too low"
-              end
-    puts message
-  else
+  if guess == number
     puts "Good job, #{name}! You guessed my number in #{guesses} guesses."
     exit
+  else
+    puts(guess > number ? 'Too high' : 'Too low')
   end
-
 end
 
 puts "Nope. The number I was thinking of was #{number}."
-
